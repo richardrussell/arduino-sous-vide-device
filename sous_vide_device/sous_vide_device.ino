6290bf9f769a8edad76d4dfb72c59c1d8d136b69
Richard Russell <richard.a.russell@gmail.com>
2012-06-17 21:07:07 +0100

    Updated (buggy) source and SConstruct file
    Currently resetting after 10 seconds and after button press

--- sous_vide_device/sous_vide_device.ino
+++ sous_vide_device/sous_vide_device.ino
@@ -3,6 +3,11 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <PID_v1.h>
+//#include <WProgram.h>
+//#include <Arduino.h>
+//#include <MemoryFree.h>
+
+
 
 // LCD is connected to pins as follows
 #define LCD_RS_PIN 7
@@ -36,8 +41,7 @@
 // temp. for nominal resistance (almost always 25 C)
 #define TEMPERATURENOMINAL 25   
 
-// how many samples to take and average, more takes longer
-// but is more 'smooth'
+// how many thermistor samples to take and average
 #define NUMSAMPLES 5
 
 // The beta coefficient of the thermistor (usually 3000-4000)
@@ -46,11 +50,11 @@
 // the value of the 'other' resistor
 #define SERIESRESISTOR 10000    
 
-int  resolution = 12;
+PROGMEM uint8_t resolution = 12;
+
 unsigned long lastTempRequest = 0;
-int  delayInMillis = 0;
+unsigned int  delayInMillis = 0;
 float temperature = 0.0;
-int  idle = 0;
 DeviceAddress tempDeviceAddress;
 
 
@@ -70,13 +74,25 @@ int relayState=LOW;
 double Setpoint = 55.0, Input, Output;
 
 //Specify the links and initial tuning parameters
-PID myPID(&Input, &Output, &Setpoint, 20, 5, 1, DIRECT);
+PID myPID(&Input, &Output, &Setpoint, 2, 1, 1, DIRECT);
 
-int WindowSize = 5000; // PWM lentgh for relay (in ms)
+PROGMEM unsigned int WindowSize = 5000; // PWM lentgh for relay (in ms)
 
 unsigned long windowStartTime;
 
+uint8_t SampleCursor = 0;
+int samples[NUMSAMPLES];
 
+float getAccurateTemp() {
+  float t1, t2;
+  t1 = getSensorTemp();
+  t2 = getThermistorTemp();
+  if (t2 - t1 > 2.0) {
+    return t1;
+  } else {
+    return t2;
+  }
+}
 
 float getSensorTemp() {
   if (millis() - lastTempRequest >= delayInMillis) // waited long enough??
@@ -86,38 +102,22 @@ float getSensorTemp() {
     delayInMillis = 750 / (1 << (12 - resolution));
     lastTempRequest = millis(); 
   }
-  
-  
-//  sensors.requestTemperatures();  // Send the command to get temperatures
   return temperature;
-//  return sensors.getTempCByIndex(0); // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
 }
 
 float getThermistorTemp() {
-  uint8_t i;
-  int samples[NUMSAMPLES];  
   float average;
+  samples[SampleCursor] = analogRead(THERMISTOR_PIN);
+  SampleCursor++;
+  SampleCursor % NUMSAMPLES;
   
-  for (i=0; i< NUMSAMPLES; i++) {
-   samples[i] = analogRead(THERMISTOR_PIN);
-   delay(10);
-  }
- 
-  // average all the samples out
   average = 0;
-  for (i=0; i< NUMSAMPLES; i++) {
+  for (uint8_t i=0; i< NUMSAMPLES; i++) {
      average += samples[i];
   }
   average /= NUMSAMPLES;
- 
-//  Serial.print("Average analog reading "); 
-//  Serial.println(average);
- 
-  // convert the value to resistance
   average = 1023 / average - 1;
   average = SERIESRESISTOR / average;
- // Serial.print("Thermistor resistance "); 
- // Serial.println(average);
  
   float steinhart;
   steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
@@ -127,9 +127,6 @@ float getThermistorTemp() {
   steinhart = 1.0 / steinhart;                 // Invert
   steinhart -= 273.15;                         // convert to C
  
- // Serial.print("Temperature "); 
- // Serial.print(steinhart);
- // Serial.println(" *C");
   return steinhart;                                      // Return the Temperature
 }
 
@@ -152,8 +149,14 @@ void setup() {
   delayInMillis = 750 / (1 << (12 - resolution)); 
   lastTempRequest = millis(); 
 
-  Serial.begin(9600);
-  Serial.println("hello serial monitor!");
+  // setup thermistor sample ring
+  for (uint8_t i=0; i< NUMSAMPLES; i++) {
+   samples[i] = analogRead(THERMISTOR_PIN);
+   delay(10);
+  }
+
+  Serial.begin(57600);
+  Serial.println("hello!");
   pinMode(BUTTON1_PIN, INPUT);
   pinMode(BUTTON2_PIN, INPUT);
   pinMode(BUTTON3_PIN, INPUT);
@@ -171,58 +174,143 @@ void setup() {
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.println("hello, world!");
+  lcd.clear();
+  lcd.println("hello");
 //  lcd.println(getThermistorTemp());
   delay(1000);
   lcd.clear();
 }
 
+boolean paused = true;
+boolean button1_pressed = false;
+boolean changeRelay = false;
+int loopCounter = 0;
+unsigned long lastLoop = millis();
+
 
 void loop() {
+  unsigned long now = millis();
+  delay(min(now-lastLoop, 100));
+  lastLoop=now;
+  loopCounter++;
+  if (loopCounter % 10 == 0) {  
+    loopCounter = 0;
+    Serial.println(now);
+  }
+  
   float thermistorTemp = getThermistorTemp(); 
   float sensorTemp = getSensorTemp();
   float ambientTemp = getAmbientTemp();
+  //  Input = (sensorTemp+thermistorTemp)/2;
+  // Input = getAccurateTemp();
+  Input = thermistorTemp;
   
   int button1_state = digitalRead(BUTTON1_PIN);
   int button2_state = digitalRead(BUTTON2_PIN);
   int button3_state = digitalRead(BUTTON3_PIN);
   
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
+//  lcd.clear();
   lcd.setCursor(0, 0);
   // print the number of seconds since reset:
   //lcd.print(millis()/1000);
   lcd.print(thermistorTemp, 1); 
-  lcd.print(" ");
+  lcd.setCursor(5, 0);
+
+//  lcd.print(" ");
   lcd.print(sensorTemp, 1); 
-  lcd.print("       ");
+  lcd.setCursor(10, 0);
+  if (paused) {
+    lcd.print("Stop");
+  } else {
+    lcd.print("Go  ");
+  }
   lcd.setCursor(0, 1);
-  lcd.print(ambientTemp, 1);
-  lcd.print(" ");
+  lcd.print(Input, 1);
+  lcd.setCursor(5, 1);
   lcd.print(Setpoint, 1);
   lcd.print(" ");
-  lcd.print(relayState);
+  lcd.print(Output, 0);
   lcd.print(" ");
-  lcd.print(button1_state);
-  lcd.print(button2_state);
-  lcd.print(button3_state);
-  lcd.print("    ");
+//  lcd.print(button1_state);
+//  lcd.print(button2_state);
+//  lcd.print(button3_state);
 
-//  Input = (sensorTemp+thermistorTemp)/2;
-  Input = thermistorTemp;
-  myPID.Compute();
+  
+//  Serial.print("freeMemory() reports ");
+//  Serial.println( freeMemory() );
+
+  if (!button2_state) {
+    Serial.println("B2");
+    Setpoint+=0.1;
+    delay(100);
+  } else if (!button3_state) {
+    Serial.println("B3");
+    Setpoint-=0.1;
+    delay(100);
+  } 
+  
+  if (!button1_state) {
+    if (!button1_pressed) {
+      Serial.println("B1");
+      button1_pressed = true;
+      if(paused) {
+        paused = false;
+      } else {
+        paused = true;
+      }
+    }
+  } else {
+    button1_pressed = false;
+  }
+  
+//  lcd.print("    ");
+  
+  if (paused) {
+//    if (loopCounter % 10 == 0) {  
+//      Serial.print("windowStartTime: ");
+//      Serial.println(windowStartTime);
+//    }
+    digitalWrite(RELAY_PIN,LOW);
+  } else {
+    myPID.Compute();
 
   /************************************************
    * turn the output pin on/off based on pid output
    ************************************************/
-  unsigned long now = millis();
-  if(now - windowStartTime>WindowSize)
-  { //time to shift the Relay Window
-    windowStartTime += WindowSize;
+    while (now - windowStartTime>WindowSize)
+    { //time to shift the Relay Window
+      windowStartTime += WindowSize;
+    }
+    
+//    if (loopCounter % 10 == 0) {  
+//      Serial.print("windowStartTime: ");
+//      Serial.println(windowStartTime);
+//      Serial.print("now:             ");
+//      Serial.println(now);
+//      Serial.print("paused:          ");
+//      Serial.println(paused);
+//    }  
+    if(Output > now - windowStartTime) {
+//       Serial.println("Relay On");
+      if (relayState == LOW) {
+        relayState=HIGH;
+        Serial.println("On");
+        changeRelay=true;
+      }
+    } else {
+//      Serial.println("Relay Off");
+      if (relayState == HIGH) {
+        relayState=LOW;
+        Serial.println("Off");
+        changeRelay=true;
+      }
+      if (changeRelay == true) {
+        digitalWrite(RELAY_PIN,relayState);
+        delay(100);
+        changeRelay = false;
+      }
+    }
   }
-  if(Output > now - windowStartTime) relayState=HIGH;
-  else relayState=LOW;
-  digitalWrite(RELAY_PIN,relayState);
 }
 
 
